8c573525631c4011af052c7f1b497808c64f1855
diegotorres9 <diegoetorres1@gmail.com>
2020-11-17 19:26:37 -0500

    new hour variable to change greeting depending on time

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ document.querySelector('button').addEventListener('click', () => {
     slideAnimation();
     rotateAnimation();
 });
-// ----------------------------------------- \\
+// --------------- FETCH TIME -------------------------- \\
 let timeUrl = 'http://worldtimeapi.org/api/ip';
 fetch(timeUrl)
 .then(res => res.json())
@@ -25,19 +25,28 @@ fetch(timeUrl)
     currentTime = new Date().toLocaleTimeString([], timeOptions);
     let timezone = `<span class="timezone">${timeData.abbreviation}</span>`;
     document.querySelector('.current-time').innerHTML = `${currentTime}${timezone}`;
+    let hour = new Date().getHours();
+    if(hour >= 5 && hour < 12) {
+        document.querySelector('.greeting').textContent = 'Good Morning';
+    }  else if(hour > 12 && hour < 18) {
+        document.querySelector('.greeting').textContent = 'Good Afternoon';
+    } else {
+        document.querySelector('.greeting').textContent = 'Good Evening';
+        document.querySelector('img.icon').src = './assets/desktop/icon-moon.svg';
+    }
     document.querySelector('.day-year').textContent = timeData.day_of_year;
     document.querySelector('.day-week').textContent = timeData.day_of_week;
     document.querySelector('.week').textContent = timeData.week_number;
     document.querySelector('.locale').textContent = timeData.timezone;
 });
-// ------------------------------------------ \\
+// -------------------- FETCH LOCATION ---------------------- \\
 let locationUrl = 'https://freegeoip.app/json/';
 fetch(locationUrl)
 .then(res => res.json())
 .then(locationData => {
     document.querySelector('.location-span').textContent = `${locationData.city}, ${locationData.region_name}`;
 });
-// --------------------------------------------- \\
+// --------------------- FETCH PROGRAMMING QUOTES ------------------------ \\
 let quoteUrl = 'http://quotes.stormconsultancy.co.uk/random.json';
 const getQuotes = async function() {
     fetch(quoteUrl)
