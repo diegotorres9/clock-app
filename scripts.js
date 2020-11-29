164985759373a3a3860131aef3eddde056cf135d
diegotorres9 <diegoetorres1@gmail.com>
2020-11-29 16:42:06 -0500

    added javascript that will change information section to black depending on time of day

--- scripts.js
+++ scripts.js
@@ -13,12 +13,33 @@ function rotateAnimation() {
 }
 document.querySelector('button').addEventListener('click', () => {
     document.querySelector('.information').classList.toggle('hide');
-    document.querySelector('.information').style.position = 'absolute';
-    document.querySelector('.information').style.bottom = '-280px';
+    // document.querySelector('.information').style.position = 'absolute';
+    // document.querySelector('.information').style.bottom = '-280px';
     slideAnimation();
     rotateAnimation();
 });
-// --------------- FETCH TIME -------------------------- \\
+// -------------- SET BACKGROUND IMAGE ----------------- \\
+// function setBgImage(){
+//     let hour = new Date().getHours();
+//     return window.innerWidth <= '375px' && hour > 18 ? document.querySelector('body').style.backgroundImage = 'url(./assets/mobile/bg-image-nighttime.jpg)'
+//             : window.innerWidth <= '768px' && hour > 18 ? document.querySelector('body').style.backgroundImage = 'url(./assets/tablet/bg-image-nighttime.jpg)'
+//             : document.querySelector('body').style.backgroundImage = 'url(./assets/desktop/bg-image-nighttime.jpg)';
+// }
+// setBgImage();
+// ------------- GET HOUR && SET GREETING--------------- \\
+let hour = new Date().getHours();
+if(hour > 5 && hour <= 11){
+    document.querySelector('.greeting').textContent = 'Good Morning';
+} else if (hour >= 12 && hour < 18){
+    document.querySelector('.greeting').textContent = 'Good Afternoon';
+} else {
+    document.querySelector('.greeting').textContent = 'Good Evening';
+    document.querySelector('.icon').src = './assets/desktop/icon-moon.svg';
+    document.querySelector('.information').style.backgroundColor = 'rgba(0,0,0, 0.75)';
+    document.querySelector('.information').style.color = '#fff';
+}
+console.log(hour);
+// --------------- FETCH TIME AND LOCATION INFORMATION -------------------------- \\
 let timeUrl = 'http://worldtimeapi.org/api/ip';
 fetch(timeUrl)
 .then(res => res.json())
@@ -28,19 +49,6 @@ fetch(timeUrl)
     currentTime = new Date().toLocaleTimeString([], timeOptions);
     let timezone = `<span class="timezone">${timeData.abbreviation}</span>`;
     document.querySelector('.current-time').innerHTML = `${currentTime}${timezone}`;
-    let hour = new Date().getHours();
-    if(hour >= 5 && hour < 12) {
-        document.querySelector('.greeting').textContent = 'Good Morning';
-    }  else if(hour > 12 && hour < 18) {
-        document.querySelector('.greeting').textContent = 'Good Afternoon';
-    } else {
-        document.querySelector('.greeting').textContent = 'Good Evening';
-        document.querySelector('img.icon').src = './assets/desktop/icon-moon.svg';
-        document.querySelector('body').style.backgroundImage = 'url(./assets/mobile/bg-image-nighttime.jpg)';
-        document.querySelector('body').style.backgroundPosition = 'top center';
-        document.querySelector('.information').style.backgroundColor = 'rgba(0,0,0, .75)';
-        document.querySelector('.information').style.color = '#fff';
-    }
     document.querySelector('.day-year').textContent = timeData.day_of_year;
     document.querySelector('.day-week').textContent = timeData.day_of_week;
     document.querySelector('.week').textContent = timeData.week_number;
